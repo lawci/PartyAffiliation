10d704021c5214a51d99b6b8f7cad143216eb382
lawci <lcft1204@gmail.com>
2025-06-22 23:58:26 +0800

    final commit

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -25,13 +25,20 @@ public class PartyAffiliation
         System.out.print("Enter your party affiliation (D, R, I): ");
         party = in.nextLine();
 
-        if (party.equals("D")) {
+        if (party.equals("D"))
+        {
             System.out.println("You get a Democratic Donkey.");
-        } else if (party.equals("R")) {
+        }
+        else if (party.equals("R"))
+        {
             System.out.println("You get a Republican Elephant.");
-        } else if (party.equals("I")) {
+        }
+        else if (party.equals("I"))
+        {
             System.out.println("You get an Independent Person.");
-        } else {
+        }
+        else
+        {
             System.out.println("You get an Other.");
         }
     }
